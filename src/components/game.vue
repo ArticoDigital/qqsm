36ed01faa7ca37c30e733062034d50f535eb854a
Juan Ramos <juan2ramos@gmail.com>
2018-10-23 13:02:36 -0500

    finish

--- src/components/game.vue
+++ src/components/game.vue
@@ -55,7 +55,7 @@
           <figure @click="nextQuestion" class="Question-correct" v-show="isQuestionCorrect">
             <img src="~@/assets/images/btnpreguntaverde.png" alt="">
             <div class="Question-content row align-center justify-center">
-              <span>¡Repuesta correcta, haz click para ir a la siguiente preguta!</span>
+              <span>¡Repuesta correcta, haz click para ir a la siguiente pregunta!</span>
             </div>
           </figure>
         </div>
